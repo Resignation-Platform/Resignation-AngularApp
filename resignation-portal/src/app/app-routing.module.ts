362f36fbb4fa9b2af950fd92982acb3053560f82
Veeranjaneyulu <vveeranjaneyulu@deloitte.com>
2022-03-07 18:47:11 +0530

    dashboadr css corrections

--- resignation-portal/src/app/app-routing.module.ts
+++ resignation-portal/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './components/dash-board/dash-board.component';
+import { ExittrackingComponent } from './components/exittracking/exittracking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard',component:DashBoardComponent},
+  {path:'exit-tracking',component:ExittrackingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- resignation-portal/src/app/components/sidebar/sidebar.component.ts
+++ resignation-portal/src/app/components/sidebar/sidebar.component.ts
@@ -7,7 +7,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  isSideBarActivated:boolean=false;
+  isSideBarActivated:boolean=true;
   constructor() { }
 
   ngOnInit(): void {
