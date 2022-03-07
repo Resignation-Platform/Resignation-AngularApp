d87796675f3363fb466f6f5142090b3dcf5f5f37
Veeranjaneyulu <vveeranjaneyulu@deloitte.com>
2022-03-07 19:29:06 +0530

    css corrections

--- resignation-portal/src/app/components/sidebar/sidebar.component.ts
+++ resignation-portal/src/app/components/sidebar/sidebar.component.ts
@@ -7,7 +7,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  isSideBarActivated:boolean=true;
+  isSideBarActivated:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
