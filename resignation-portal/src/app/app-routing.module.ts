d96918b19f4dabed60f61dd7b22b6c27a386ad03
Veeranjaneyulu <vveeranjaneyulu@deloitte.com>
2022-03-08 10:22:30 +0530

    combined all the flows

--- resignation-portal/src/app/app-routing.module.ts
+++ resignation-portal/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdmindashboardComponent } from './components/admindashboard/admindashboard.component';
 import { DashBoardComponent } from './components/dash-board/dash-board.component';
 import { ExittrackingComponent } from './components/exittracking/exittracking.component';
 import { LoginComponent } from './components/login/login.component';
+import { ResignationFormComponent } from './components/resignation-form/resignation-form.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
   {path:'dashboard',component:DashBoardComponent},
   {path:'exit-tracking',component:ExittrackingComponent},
-  { path: 'login', component: LoginComponent},
+  {path:'seperation',component:ResignationFormComponent},
+  {path:'admin', component:AdmindashboardComponent}
+
 ];
 
 @NgModule({
