4f5a803ee696c82a5c5f05410068821ae01e8ac6
sk9749992 <sk9749992@gmail.com>
2022-03-09 13:58:00 +0530

    added mock data

--- resignation-portal/src/app/components/dash-board/dash-board.component.spec.ts
+++ resignation-portal/src/app/components/dash-board/dash-board.component.spec.ts
@@ -1,25 +1,49 @@
+import { Injector } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ResignationService } from 'src/services/resignation.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DashBoardComponent } from './dash-board.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EmployeeMockData } from 'src/app/mockData/employeeData';
 
 describe('DashBoardComponent', () => {
   let component: DashBoardComponent;
   let fixture: ComponentFixture<DashBoardComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DashBoardComponent ]
-    })
-    .compileComponents();
-  });
+  let injector: Injector;
+  let resignationService: ResignationService;
+  const details = {
+    empNumber: '77723',
+    empName: 'Test User',
+    empRole: 'developer',
+  };
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      declarations: [DashBoardComponent],
+      providers: [ResignationService, EmployeeMockData],
+    });
     fixture = TestBed.createComponent(DashBoardComponent);
     component = fixture.componentInstance;
+    injector = fixture.debugElement.injector;
+    resignationService = injector.get(ResignationService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get value of empName and empNumber from local storage on calling ngOnInit', () => {
+    let details = {
+      empNumber: '77723',
+      empName: 'Test User',
+      empRole: 'developer',
+    };
+    localStorage.setItem('Employee_Details', JSON.stringify(details));
+    component.ngOnInit();
+    expect(component.empName).toBe('Test User');
+    expect(component.empNumber).toBe('77723');
+    localStorage.clear();
   });
 });
